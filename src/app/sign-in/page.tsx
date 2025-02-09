8dd0ceafc2fc31f112797c428a5e2cc7c0216248
jorgeabrahan <jorge24abrahan@gmail.com>
2025-02-08 23:44:18 -0600

    Fix bug when viewing forms with phone horizontally

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 export default function PageSignIn() {
   return (
-    <WrapperDelimiter className='h-screen flex flex-col justify-center'>
+    <WrapperDelimiter className='min-h-screen flex flex-col justify-center'>
       <div className='bg-night-600 pt-4 pb-8 px-4 rounded-lg space-y-6 w-full max-w-[500px] mx-auto'>
         <h1 className='text-4xl font-bold text-center'>Sign In</h1>
         <form className='space-y-4'>
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 export default function PageSignUp() {
   return (
-    <WrapperDelimiter className='h-screen flex flex-col justify-center'>
+    <WrapperDelimiter className='min-h-screen flex flex-col justify-center'>
       <div className='bg-night-600 pt-4 pb-8 px-4 rounded-lg space-y-6 w-full max-w-[500px] mx-auto'>
         <h1 className='text-4xl font-bold text-center'>Sign Up</h1>
         <form className='space-y-4'>
