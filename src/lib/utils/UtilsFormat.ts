17e2ee46dcccc75b9da215ff99ad787dc1c40c22
jorgeabrahan <jorge24abrahan@gmail.com>
2025-02-11 03:38:01 -0600

    Create format utils class with capitalize words static method

---
+++ src/lib/utils/UtilsFormat.ts
@@ -0,0 +1,8 @@
+export class UtilsFormat {
+  static capitalizeWords(input: string): string {
+    return input
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ')
+  }
+}
