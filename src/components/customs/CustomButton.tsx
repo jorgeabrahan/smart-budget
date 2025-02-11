962288b6d11fd216ed4a92fb77025d7324d2d524
jorgeabrahan <jorge24abrahan@gmail.com>
2025-02-11 03:40:54 -0600

    Move custom button and input to custom components folder

--- src/components/customs/CustomButton.tsx
+++ src/components/customs/CustomButton.tsx
@@ -2,16 +2,21 @@ export default function CustomButton({
   className = '',
   type = 'button',
   children,
+  disabled = false,
   ...props
-}: {
+}: React.ButtonHTMLAttributes<HTMLButtonElement> & {
   className?: string
   type?: 'button' | 'submit'
   children: React.ReactNode
+  disabled?: boolean
 }) {
   return (
     <button
-      className={`px-5 py-2 rounded-lg text-sm font-semibold ${className}`}
+      className={`px-5 py-2 rounded-lg text-sm font-semibold ${
+        disabled && 'opacity-50'
+      } ${className}`}
       type={type}
+      disabled={disabled}
       {...props}
     >
       {children}
--- src/components/customs/CustomInput.tsx
+++ src/components/customs/CustomInput.tsx
@@ -6,15 +6,18 @@ import { useState } from 'react'
 export default function CustomInput({
   label,
   id,
-  type = 'text'
-}: {
+  type = 'text',
+  disabled = false,
+  ...props
+}: React.InputHTMLAttributes<HTMLInputElement> & {
   label: string
   id: string
   type?: 'text' | 'password' | 'email'
+  disabled?: boolean
 }) {
   const [isShowingPassword, setIsShowingPassword] = useState(false)
   return (
-    <div className='relative'>
+    <div className={`relative ${disabled && 'opacity-50'}`}>
       <label
         htmlFor={id}
         className='absolute left-3 top-3 text-xs text-white/40'
@@ -22,11 +25,13 @@ export default function CustomInput({
         {label}
       </label>
       <input
-        className='bg-night-700 block w-full px-3 pt-8 pb-3 text-sm outline-none rounded-lg border border-white/40 focus:border-white/80'
+        className='bg-night-700 block w-full px-3 pt-8 pb-3 text-sm outline-none rounded-lg border border-white/40 focus:border-white/80 transition-colors duration-300'
         id={id}
         name={id}
         autoComplete='off'
         type={isShowingPassword ? 'text' : type}
+        disabled={disabled}
+        {...props}
       />
       {type === 'password' && (
         <button
